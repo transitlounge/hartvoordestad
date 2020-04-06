5f7543522a2639a1fde9c8fd5f9c2c21f8589ffd
Marijn DVH <marijn@marinos.nl>
2020-04-06 21:53:42 +0800

    auth + weird duplication fix

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,8 +12,8 @@
         <!-- Styles -->
         <style>
             html, body {
-                background-color: #fff;
-                color: #636b6f;
+                background-color: #ec0000;
+                color: white;
                 font-family: 'Nunito', sans-serif;
                 font-weight: 200;
                 height: 100vh;
@@ -81,19 +81,11 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                   XXX Hartvoordestad XXX
                 </div>
+                <em>Binnenkort kun je hier je hart luchten!</em>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+
             </div>
         </div>
     </body>
